a93f45f05ed97eafd0059ed659256943802d65b1
XX <XX@DESKTOP-RD52DAH>
2022-11-17 16:54:10 +0800

    1

--- turbo/int_state.m
+++
@@ -1,7 +0,0 @@
-function int_state = int_state( state )
-% converts a row vector of m bits into a integer (base 10)
-[dummy, m] = size( state );
-for i = 1:m
-vect(i) = 2^(m-i);
-end
-int_state = state*vect';
